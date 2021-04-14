7cad59f1dba48a565735be1eda236d97d8ce1ef0
Stalex66 <60701933+Stalex66@users.noreply.github.com>
2021-04-14 14:03:06 +0200

    0.0.01 test

--- game.h
+++ game.h
@@ -1,1 +1,12 @@
-//
+#include <iostream>
+#include <memory>
+#include <string>
+#include "game.cpp"
+
+
+class Game{
+  string name;
+  weak_ptr<Player> host;
+  map<string,weak_ptr<Player>> players;
+
+}
